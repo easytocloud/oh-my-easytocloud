ee46d71b268d488e4b894489250676a908cec1bb
Erik Meinders <erik@easytocloud.com>
2022-03-28 14:28:57 +0200

    Initial version of custom/theme for oh-my-zsh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+sudo yum install zsh
+chch -c /bin/zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd ~/.oh-my-zsh/custom/themes
+curl https://github.com/easytocloud/oh-my-cloud9/custom/themes/easytocloud.zsh-theme -o .
+
